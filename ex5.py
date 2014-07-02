b5b56259b853938e8b2a3147871c5552393dc4f6
Savannah Henderson <savannahjune@gmail.com>
2014-07-02 13:19:07 -0700

    exercise 5

---
+++ ex5.py
@@ -0,0 +1,38 @@
+#Ex number 5
+
+my_name = 'Zed A. Shaw'
+my_age = 35 # not alie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name 
+print "He's %d inches tall." % my_height 
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d."% (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+#study drills
+
+my_name = 'Savannah'
+my_age = 26 # not alie
+my_height = 69 # inches
+my_weight = 160 # lbs
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name 
+print "She's %d inches tall." % my_height 
+print "She's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the tea." % my_teeth
+
+#thoughts: %s is about a string, %d is for a number
